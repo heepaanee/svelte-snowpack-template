260676e0e3900074046bb8fd7776c95204fe32c5
heepaanee <heepan.kim@gmail.com>
2021-01-27 12:01:57 +0900

    config Footer.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,14 @@
   import Router from "svelte-spa-router";
   import routes from "~/routes";
   import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
 <Header />
 
 <Router {routes} />
+
+<Footer />
 
 <style lang="scss">
   // If you want to import the `scss/main.scss` file globally,
