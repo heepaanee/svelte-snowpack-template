e989459087be8bbc2caf472a6fc2b7089954954d
heepaanee <heepan.kim@gmail.com>
2021-01-27 11:28:10 +0900

    router params

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,12 +2,38 @@
   import { link } from "svelte-spa-router";
   import active from "svelte-spa-router/active";
   import Logo from "~/components/Logo.svelte";
+
+  const menus = [
+    {
+      href: "/",
+      name: "Search",
+    },
+    {
+      href: "/movie/tt4520988",
+      name: "Movie",
+      path: "/movie/*",
+    },
+    {
+      href: "/about",
+      name: "About",
+      path: /^\/about/, // 정규표현식 사용!
+    },
+  ];
 </script>
 
 <header>
   <Logo />
-  <a use:link use:active={"/"} href="/"> home </a>
-  <a use:link use:active={"/about"} href="/about">about</a>
+  <nav>
+    <ul>
+      {#each menus as { href, name, path } (name)}
+        <li>
+          <a use:link use:active={path} {href}>
+            {name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
 </header>
 
 <style lang="scss">
