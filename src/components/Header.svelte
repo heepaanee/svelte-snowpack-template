ca52411d6de022f3782f15c7b07c46c56fdd8c06
heepaanee <heepan.kim@gmail.com>
2021-01-27 10:56:52 +0900

    apply Logo

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import { link } from "svelte-spa-router";
   import active from "svelte-spa-router/active";
+  import Logo from "~/components/Logo.svelte";
 </script>
 
 <header>
-  #### HEADER
-
+  <Logo />
   <a use:link use:active={"/"} href="/"> home </a>
   <a use:link use:active={"/about"} href="/about">about</a>
 </header>
